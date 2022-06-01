ce47bf32d2ae06685ee492c95bae4481a607a0cc
andy-shi88 <shi88.andy@gmail.com>
2022-06-01 09:15:55 +0700

    add missing toolbar.items

--- resources/js/ckeditor/config/toolbar.js
+++ resources/js/ckeditor/config/toolbar.js
@@ -25,6 +25,8 @@ export default {
             '|',
             'snippetBrowser',
             'htmlEmbed',
+            'sourceEditing',
+            'codeBlock',
             '|',
             'undo',
             'redo'
